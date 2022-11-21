ddb5a804657b7ece2bb80ca4b35e09dfd0c8921c
Haig Gregory Agdere <hgagdere@gmail.com>
2022-11-21 11:00:18 -0500

    Send email cron job

--- cron/sendSubscriptionEmails.js
+++ cron/sendSubscriptionEmails.js
@@ -1,8 +1,13 @@
-(async function run() {
+const fetch = require('node-fetch')
+
+async function run() {
+    // fetch('http://localhost:3000/api/send-subscription-email', {
     fetch('https://react-job-board.herokuapp.com/api/send-subscription-email', {
         method: 'POST',
         body: {}
     }).then(res => {
         res.json().then(data => console.log('data: ', data))
     }).catch(err => console.log("ERROR"))
-})()
+}
+
+run()
