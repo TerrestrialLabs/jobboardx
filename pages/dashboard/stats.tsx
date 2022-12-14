faae06a26648647fcc1411ec3027e61e7dac336e
Haig Gregory Agdere <hgagdere@gmail.com>
2022-12-14 11:28:34 -0500

    Mobile stats layout

--- pages/dashboard/stats.tsx
+++ pages/dashboard/stats.tsx
@@ -52,20 +52,20 @@ const Stats: NextPage = () => {
                             </Grid>
 
                             <Grid xs={12} container>
-                                <Grid pr={4} xs={6} sm={4}>
-                                    <Box pt={2} sx={{ backgroundColor: '#fff', borderRadius: 1, borderRight: '1px solid #e7e7e7' }}>
+                                <Grid pr={mobile ? 0 : 4} xs={12} sm={4}>
+                                    <Box display='flex' flexDirection='column' alignItems={mobile ? 'center' : 'flex-start'} pt={2} sx={{ backgroundColor: '#fff', borderRadius: 1, borderRight: mobile ? 0 : '1px solid #e7e7e7' }}>
                                         <Box mb={0.75}><Typography>Job views</Typography></Box>
                                         <Box><Typography color='primary' fontSize={24} fontWeight='bold'>{data ? data.views : 0}</Typography></Box>
                                     </Box>
                                 </Grid>
-                                <Grid xs={6} sm={4}>
-                                    <Box pt={2} pr={4} sx={{ backgroundColor: '#fff', borderRadius: 1, borderRight: '1px solid #e7e7e7' }}>
+                                <Grid xs={12} sm={4}>
+                                    <Box display='flex' flexDirection='column' alignItems={mobile ? 'center' : 'flex-start'} pt={2} pr={mobile ? 0 : 4} sx={{ backgroundColor: '#fff', borderRadius: 1, borderRight: mobile ? 0 : '1px solid #e7e7e7' }}>
                                         <Box><Typography mb={0.75}>Applications via link</Typography></Box>
                                         <Box><Typography color='primary' fontSize={24} fontWeight='bold'>{data ? data.urlApplyClicks : 0}</Typography></Box>
                                     </Box>
                                 </Grid>
-                                <Grid pt={2} pl={4} xs={6} sm={4}>
-                                    <Box sx={{ backgroundColor: '#fff', borderRadius: 1 }}>
+                                <Grid pt={2} pl={mobile ? 0 : 4} xs={12} sm={4}>
+                                    <Box display='flex' flexDirection='column' alignItems={mobile ? 'center' : 'flex-start'} sx={{ backgroundColor: '#fff', borderRadius: 1 }}>
                                         <Box><Typography mb={0.75}>Applications via email</Typography></Box>
                                         <Box><Typography color='primary' fontSize={24} fontWeight='bold'>{data ? data.emailApplyClicks : 0}</Typography></Box>
                                     </Box>
@@ -75,7 +75,7 @@ const Stats: NextPage = () => {
                     </Grid>
 
                     <Grid xs={12}>
-                        <Box sx={{ backgroundColor: '#fff', width: '100%'}} p={mobile ? 2 : 4}>
+                        <Box sx={{ backgroundColor: '#fff', borderRadius: 1, width: '100%'}} p={mobile ? 2 : 4}>
                             <Grid xs={12}>
                                 <Box>
                                     <Typography fontWeight='bold'>Total</Typography>
@@ -83,8 +83,8 @@ const Stats: NextPage = () => {
                             </Grid>
 
                             <Grid xs={12} container>
-                                <Grid xs={4} pr={4}>
-                                    <Box pt={2} sx={{ backgroundColor: '#fff', borderRadius: 1, borderRight: '1px solid #e7e7e7' }}>
+                                <Grid xs={12} sm={4} pr={4}>
+                                    <Box pt={2} display='flex' flexDirection='column' alignItems={mobile ? 'center' : 'flex-start'} sx={{ backgroundColor: '#fff', borderRadius: 1, borderRight: mobile ? 0 : '1px solid #e7e7e7' }}>
                                         <Box mb={0.75}><Typography>Jobs posted</Typography></Box>
                                         <Box><Typography color='primary' fontSize={24} fontWeight='bold'>{data ? data.jobs : 0}</Typography></Box>
                                     </Box>
