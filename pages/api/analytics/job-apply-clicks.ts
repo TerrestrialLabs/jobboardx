2458bb44b578ce2c7580be09ca3a1c06c92ae5ef
Haig Gregory Agdere <hgagdere@gmail.com>
2022-12-15 15:45:52 -0500

    Don't create user events for employers or admins

--- pages/api/analytics/job-apply-clicks.ts
+++ pages/api/analytics/job-apply-clicks.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import dbConnect from '../../../mongodb/dbconnect'
 import UserEvent from '../../../models/UserEvent'
+import { getSession } from 'next-auth/react'
 
 function getErrorMessage(error: unknown) {
     if (error instanceof Error) { 
@@ -19,6 +20,19 @@ export default async function handler(
     
     if (method === 'POST') {
         try {
+            const session = await getSession({ req })
+            // @ts-ignore
+            if (!session?.user || (session?.user.role !== 'employer' && session?.user.role !== 'admin')) {
+                const ipAddress = req.headers['x-forwarded-for']  || req.socket.remoteAddress
+
+                await UserEvent.create({
+                    jobboardId: req.body.jobboardId,
+                    jobId: req.body.jobId,
+                    type: 'job-view',
+                    ipAddress
+                })
+            }
+
             const ipAddress = req.headers['x-forwarded-for']  || req.socket.remoteAddress
 
             await UserEvent.create({
--- pages/api/analytics/job-views.ts
+++ pages/api/analytics/job-views.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import dbConnect from '../../../mongodb/dbconnect'
 import UserEvent from '../../../models/UserEvent'
+import { getSession } from 'next-auth/react'
 
 function getErrorMessage(error: unknown) {
     if (error instanceof Error) { 
@@ -19,14 +20,18 @@ export default async function handler(
 
     if (method === 'POST') {
         try {
-            const ipAddress = req.headers['x-forwarded-for']  || req.socket.remoteAddress
+            const session = await getSession({ req })
+            // @ts-ignore
+            if (!session?.user || (session?.user.role !== 'employer' && session?.user.role !== 'admin')) {
+                const ipAddress = req.headers['x-forwarded-for']  || req.socket.remoteAddress
 
-            await UserEvent.create({
-                jobboardId: req.body.jobboardId,
-                jobId: req.body.jobId,
-                type: 'job-view',
-                ipAddress
-            })
+                await UserEvent.create({
+                    jobboardId: req.body.jobboardId,
+                    jobId: req.body.jobId,
+                    type: 'job-view',
+                    ipAddress
+                })
+            }
 
             res.status(201).json(true)
         } catch(err) {
