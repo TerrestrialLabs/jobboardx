9118aa467494da6b9a5bb4fd69b94fce010fa913
Haig Gregory Agdere <hgagdere@gmail.com>
2022-12-16 11:08:00 -0500

    Fix header menu bug

--- components/Header.tsx
+++ components/Header.tsx
@@ -57,8 +57,8 @@ const Header = () => {
             sx={{ fontSize: '14px', marginTop: '0.5rem' }}
         >
             <Box p='6px 16px' mb='0.5rem' sx={{ width: '170px', borderBottom: '1px solid #e7e7e7' }}><Typography fontWeight='bold'>Employers</Typography></Box>
-            {session && <MenuItem onClick={handleAccountMenuClose}><Link href='/dashboard'>Dashboard</Link></MenuItem>}
-            {session && <MenuItem onClick={handleAccountMenuClose}><Link href='/logout'>Logout</Link></MenuItem>}
+            {session?.user && <MenuItem onClick={handleAccountMenuClose}><Link href='/dashboard'>Dashboard</Link></MenuItem>}
+            {session?.user && <MenuItem onClick={handleAccountMenuClose}><Link href='/logout'>Logout</Link></MenuItem>}
 
             {(!session || !session.user) && <MenuItem onClick={handleAccountMenuClose}><Link href='/login'>Log in</Link></MenuItem>}
             {(!session || !session.user) && <MenuItem onClick={handleAccountMenuClose}><Link href='/signup'>Sign up</Link></MenuItem>}
