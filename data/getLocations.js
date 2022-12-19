2bcbddfe612d29931ba7a1b29259efe83be9681f
Haig Gregory Agdere <hgagdere@gmail.com>
2022-12-19 12:19:54 -0500

    Only jobs in USA for now

--- data/getLocations.js
+++ data/getLocations.js
@@ -1,38 +1,56 @@
-const fs = require('fs')
-var cities = require('./cities.json')
+import fs from 'fs'
+// const fs = require('fs')
+// var cities = require('./cities.json')
+var cities = require('./world_cities.json')
 
-function getCities() {
-  let americanCities = []
-  let worldCities = []
+export function getLocations() {
+  // let americanCities = []
+  // let worldCities = []
 
-  const supportedCountries = [
-    'United States',
-    'Canada',
-    'United Kingdom',
-    'Ireland',
-    'Australia',
-    'New Zealand'
-  ]
+  // const supportedCountries = [
+    // 'United States',
+    // 'Canada',
+    // 'United Kingdom',
+    // 'Ireland',
+    // 'Australia',
+    // 'New Zealand'
+  // ]
 
-  cities.forEach(city => {
-    if (city.country_name === "United States") {
-      americanCities.push(city)
-    }
+  // cities.forEach(city => {
+  //   if (city.country_name === "United States") {
+  //     americanCities.push(city)
+  //   }
 
-    if (supportedCountries.indexOf(city.country_name) > -1) {
-      worldCities.push(city)
-    }
+  //   if (supportedCountries.indexOf(city.country_name) > -1) {
+  //     worldCities.push(city)
+  //   }
+  // })
+
+  // const worldCitiesValues = worldCities.map(city => {
+  //   if (city.country_name === 'United States') {
+  //     return `${city.name}, ${city.state_code}`
+  //   } else {
+  //     return `${city.name}, ${city.country_name}`
+  //   }
+  // })
+
+  // fs.writeFile('./world_cities.json', JSON.stringify(worldCitiesValues), err => {
+  //   if (err) {
+  //     console.log(err)
+  //   } else {
+  //     console.log('JSON file written')
+  //   }
+  // })
+
+  const cityOptions = cities.filter(city => {
+    return !city.includes(', Canada') && 
+      !city.includes(', United Kingdom') && 
+      !city.includes(', Ireland') && 
+      !city.includes(', Australia') && 
+      !city.includes(', New Zealand')
   })
 
-  const worldCitiesValues = worldCities.map(city => {
-    if (city.country_name === 'United States') {
-      return `${city.name}, ${city.state_code}`
-    } else {
-      return `${city.name}, ${city.country_name}`
-    }
-  })
-
-  fs.writeFile('./world_cities.json', JSON.stringify(worldCitiesValues), err => {
+  fs.writeFile('./world_cities_options.json', JSON.stringify(cityOptions), err => {
     if (err) {
       console.log(err)
     } else {
@@ -40,5 +58,3 @@ function getCities() {
     }
   })
 }
-
-getCities()
