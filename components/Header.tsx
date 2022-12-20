492d57ee6a17efa8d71d0048d237e40e936724b3
Haig Gregory Agdere <hgagdere@gmail.com>
2022-12-20 09:50:24 -0500

    Nav bar employer menu icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useContext, useState } from 'react'
 import { JobBoardContext, JobBoardContextValue } from '../context/JobBoardContext'
-import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+import { AccountCircle, GridView, Key, Logout, PersonAddAlt } from '@mui/icons-material'
 import { useWindowSize } from '../hooks/hooks'
 import { useSession } from 'next-auth/react'
 import { ROLE } from '../const/const'
@@ -63,11 +63,48 @@ const Header = () => {
             sx={{ fontSize: '14px', marginTop: '0.5rem' }}
         >
             <Box p='6px 16px' mb='0.5rem' sx={{ width: '170px', borderBottom: '1px solid #e7e7e7' }}><Typography fontWeight='bold'>{isAdmin ? 'Admin' : 'Employers'}</Typography></Box>
-            {session?.user && isEmployer && <MenuItem onClick={handleAccountMenuClose}><Link href='/dashboard'>Dashboard</Link></MenuItem>}
-            {session?.user && <MenuItem onClick={handleAccountMenuClose}><Link href='/logout'>Logout</Link></MenuItem>}
 
-            {(!session || !session.user) && <MenuItem onClick={handleAccountMenuClose}><Link href='/login'>Log in</Link></MenuItem>}
-            {(!session || !session.user) && <MenuItem onClick={handleAccountMenuClose}><Link href='/signup'>Sign up</Link></MenuItem>}
+            {session?.user && isEmployer && (
+                <MenuItem onClick={handleAccountMenuClose}>
+                    <Link href='/dashboard'>
+                        <Box display='flex'>
+                            <GridView />
+                            <Typography ml={1}>Dashboard</Typography>
+                        </Box>
+                    </Link>
+                </MenuItem>
+            )}
+
+            {session?.user && (
+                <MenuItem onClick={handleAccountMenuClose}>
+                    <Link href='/logout'>
+                        <Box display='flex'>
+                            <Logout />
+                            <Typography ml={1}>Logout</Typography>
+                        </Box>
+                    </Link>
+                </MenuItem>
+            )}
+
+            {(!session || !session.user) && (
+                <MenuItem onClick={handleAccountMenuClose}>
+                    <Link href='/login'>
+                        <Box display='flex'>
+                            <Key />
+                            <Typography ml={1}>Log in</Typography>
+                        </Box>
+                    </Link>
+                </MenuItem>
+            )}
+
+            {(!session || !session.user) && (<MenuItem onClick={handleAccountMenuClose}>
+                <Link href='/signup'>
+                    <Box display='flex'>
+                        <PersonAddAlt />
+                        <Typography ml={1}>Sign up</Typography>
+                    </Box>
+                </Link>
+            </MenuItem>)}
         </Menu>
     )
 
@@ -100,7 +137,7 @@ const Header = () => {
                                 onClick={handleAccountButtonClick}
                                 sx={{ backgroundColor: '#fff', padding: 0, marginRight: 2, "&:hover": { backgroundColor: "#fff" } }}
                             >
-                                <AccountCircleIcon fontSize='large' color='secondary' />
+                                <AccountCircle fontSize='large' color='secondary' />
                             </IconButton>
 
                             {menu}
